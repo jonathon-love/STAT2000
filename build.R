4e0b65c33d5b2944e42bd275cbeb2fc396de1f2e
VLC-05L <60168194+VLC-05L@users.noreply.github.com>
2020-02-21 16:51:22 +1100

    Data files, journal links
    Changed the journal links back to the proper journal sites. Added the data files so that they would be embedded in the document.

--- build.R
+++ build.R
@@ -11,11 +11,18 @@ if ( ! require(rmarkdown))
 
 Rmds <- list.files(pattern='.*\\.Rmd')
 
+# stri_rand_strings(12, sample(4:6, 12, replace=TRUE))
+rand_sol_code = c("wqib",   "UnxCQQ", "sdLi",   "0ySXQ",  "e3nP",   "80WX",   
+              "zxOl",   "zoZt",   "ycV8Ky", "SJwwO",  "iKKpMc", "DwDx")
+
+Rmd_index = 1:length(Rmds)
+
 for (Rmd in Rmds) {
   match <- regexec('^(.*)\\.Rmd$', Rmd)
   name <- substring(Rmd, 1, nchar(Rmd)-4)
-  output_q <- paste(name, 'Q.html')
-  output_s <- paste(name, 'S.html')
+  output_q <- paste(name, ' Q.html', sep="")
+  output_s <- paste(name, ' S.html', sep="")
+#  output_s <- paste(name, ' S ',rand_sol_code[Rmd_index[Rmd == Rmds]],'.html', sep="")
 
   rmarkdown::render(
     input=Rmd,
